91d3317158709b9a288e52e535a0e6d64c1a16c6
carloseduribeiro <carloseribeiro96@gmail.com>
2024-02-06 23:56:05 -0300

    Adding Category Resolvers

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"github.com/carloseduribeiro/working-with-graphql-go/internal/database"
 	"log"
 	"net/http"
 	"os"
@@ -8,17 +10,27 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/carloseduribeiro/working-with-graphql-go/graph"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	db, err := sql.Open("sqlite3", "data.db")
+	if err != nil {
+		log.Fatalf("failed to open database: %v", err)
+	}
+	defer db.Close()
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	categoryDB := database.NewCategory(db)
+	resolver := &graph.Resolver{CategoryDB: categoryDB}
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: resolver}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
